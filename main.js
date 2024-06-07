c24c4ed85c0a1a9495ba509140845ef29eb67f99
Justin-Akridge <akridjus@gmail.com>
2024-06-07 15:54:24 -0400

    updates

--- main.js
+++ main.js
@@ -70,20 +70,356 @@ let width = window.innerWidth;
 let height = window.innerHeight;
 let points; 
 const positions = [];
+// function addMarkers(poleLocations) {
+//     poleLocations.forEach(location => {
+//         const position = new THREE.Vector3(location.x, location.y, location.z);
+//         const color = new THREE.Color(0xff0000);
+//         const size = 0.7;
+
+//         // Create marker
+//         const marker = new Potree.Marker(potreeViewer.scene.scene, position, color, size);
+//         marker.description = `Marker at (${location.x}, ${location.y}, ${location.z})`;
+
+//         // Add marker to Potree scene
+//         potreeViewer.scene.addMarker(marker);
+//     });
+// }
+// const measure = new Potree.Measure();
+// potreeViewer.scene.scene.add(measure); // Add the measure object to the scene
+
+// function addMarkers(poleLocations) {
+//     poleLocations.forEach(location => {
+//         const position = new THREE.Vector3(location.x, location.y, location.z);
+//         measure.addMarker(position);
+
+//         // Optionally log positions for debugging
+//         console.log('Added marker at:', position);
+//     });
+
+//     // Ensure the measure object is visible
+//     measure.visible = true;
+//     measure.updateMatrixWorld(true);
+// }
+// const measure = new Potree.Measure();
+
+// function addMarkers(poleLocations) {
+//     poleLocations.forEach(location => {
+//         measure.addMarker(new THREE.Vector3(location.x, location.y, location.z));
+//     });
+//     potreeViewer.scene.scene.add(measure);
+// }
+// potreeViewer.inputHandler.registerInteractiveScene(potreeViewer.scene);
+
+// potreeViewer.addEventListener('click', event => {
+//     const selectedAnnotation = event.intersection ? event.intersection.object : null;
+//     if (selectedAnnotation && selectedAnnotation instanceof Measure) {
+//         console.log("Annotation clicked:", selectedAnnotation);
+//     }
+// });
+// function addMarkers(poleLocations) {
+//     poleLocations.forEach(location => {
+//         const position = new THREE.Vector3(location.x, location.y, location.z);
+
+//         // Create a custom circle geometry
+//         const geometry = new THREE.CircleGeometry(0.5, 32);
+//         const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+//         const circle = new THREE.Mesh(geometry, material);
+//         circle.position.copy(position);
+//         circle.rotation.x = -Math.PI / 2; // Rotate to face upwards
+
+//         // Create a Potree annotation with the custom circle
+//         const annotation = new Potree.Annotation({
+//             position: position,
+//             title: 'Pole',
+//             description: 'Pole marker',
+//             cameraPosition: position.clone().add(new THREE.Vector3(0, 0, 10)),
+//             scale: 1,
+//             elements: [circle] // Add the circle as a custom element
+//         });
+
+//         // Add the annotation to the Potree scene
+//         potreeViewer.scene.annotations.add(annotation);
+//     });
+// }
+
+// function addMarkers(poleLocations) {
+//     poleLocations.forEach(location => {
+//         const position = new THREE.Vector3(location.x, location.y, location.z);
+
+//         // Create a Potree annotation
+//         const annotation = new Potree.Annotation({
+//             position: position,
+//             cameraPosition: position.clone().add(new THREE.Vector3(0, 0, 10)),
+//             scale: 1,
+//             color: 0xff0000
+//         });
+
+//         // Add the annotation to the Potree scene
+//         potreeViewer.scene.annotations.add(annotation);
+//     });
+// }
+// function addMarkers(poleLocations) {
+//     poleLocations.forEach(location => {
+//         const position = new THREE.Vector3(location.x, location.y, location.z);
+
+//         // Create a Potree marker
+//         const marker = new Potree.Marker();
+//         marker.position.set(location.x, location.y, location.z);
+//         marker.setLabel('Pole');
+//         marker.setColor(0xff0000); // Set the marker color to red
+//         marker.setScale(1); // Set the marker scale
+
+//         // Add the marker to the Potree scene
+//         potreeViewer.scene.addMarker(marker);
+//     });
+// }
+
+// function addMarkers(poleLocations) {
+//     const markers = []; // Array to store Marker instances
+
+//     poleLocations.forEach(location => {
+//         // Create a new instance of the Marker class
+// 		console.log(location)
+//         const marker = new Potree.Marker();
+// 		console.log(marker)
+        
+//         // Set position and color properties for the marker
+//         marker.setPosition(location);
+//         marker.setColor(new THREE.Color(0xff0000)); // Example color
+        
+//         // Add the marker to the scene or any other container
+//         scene.add(marker);
+
+//         // Optionally, store the marker in an array for later reference
+//         markers.push(marker);
+//     });
+// }
+// function addMarkers(poleLocations) {
+//     const spheres = [];
+//     const points = [];
+
+//     poleLocations.forEach(location => {
+//         points.push(location);
+
+//         const geometry = new THREE.SphereGeometry(0.7, 32, 32);
+//         const material = new THREE.MeshPhongMaterial({ color: 'red' });
+//         const sphere = new THREE.Mesh(geometry, material);
+
+
+//         sphere.position.set(location.x, location.y, location.z);
+
+// 		potreeViewer.scene.scene.add(sphere);
+// 		spheres.push(sphere);
+//         // Add event listeners with function binding
+//     });
+// }
+
+let markers = []
+let spheres = []
+let draggingEnabled = false; // Flag to indicate whether dragging and dropping is enabled
+
+// Function to toggle draggingEnabled flag
+function toggleDragging() {
+    draggingEnabled = !draggingEnabled; // Toggle the flag
+    const buttonText = draggingEnabled ? 'Disable Drag and Drop' : 'Enable Drag and Drop';
+    document.getElementById('toggleButton').innerText = buttonText; // Update button text
+}
+
+// Add click event listener to the button
+document.getElementById('toggleButton').addEventListener('click', toggleDragging);
+
+// Function to handle drag event
+// const drag = (e) => {
+//     if (draggingEnabled) { 
+// 		let I = Potree.Utils.getMousePointCloudIntersection(
+// 			e.drag.end, 
+// 			e.viewer.scene.getActiveCamera(), 
+// 			e.viewer, 
+// 			e.viewer.scene.pointclouds,
+// 			{pickClipped: true}
+// 		);
+
+// 		if (I) {
+// 			let i = spheres.indexOf(e.drag.object);
+// 			if (i !== -1) {
+// 				let point = points[i];
+
+// 				// loop through current keys and cleanup ones that will be orphaned
+// 				for (let key of Object.keys(point)) {
+// 					if (!I.point[key]) {
+// 						delete point[key];
+// 					}
+// 				}
+
+// 				for (let key of Object.keys(I.point).filter(e => e !== 'position')) {
+// 					point[key] = I.point[key];
+// 				}
+
+// 				sphere.position.set(I.location.x, I.location.y, I.location.z);
+// 				// this.setPosition(i, I.location); // Here, `this` should refer to the instance of the Measure class
+// 			}
+// 		}
+// 	};
+// }
+
+// // Function to handle drop event
+// const drop = (e) => {
+//     if (draggingEnabled) { // Check if dragging is enabled
+//         // Your drop event handling logic here
+//     }
+// };
+
+// // Function to handle mouseover event
+// const mouseover = (e) => {
+//     if (draggingEnabled) { // Check if dragging is enabled
+//         // Your mouseover event handling logic here
+//     }
+// };
+
+// // Function to handle mouseleave event
+// const mouseleave = (e) => {
+//     if (draggingEnabled) { // Check if dragging is enabled
+//         // Your mouseleave event handling logic here
+//     }
+// };
+
+// // Iterate over pole locations and create spheres
+// function addMarkers(poleLocations) {
+// 	poleLocations.forEach(location => {
+// 		const geometry = new THREE.CylinderGeometry(0.2, 0.2, 22, 6);
+// 		const material = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+// 		// const material = new THREE.MeshPhongMaterial();
+// 		const sphere = new THREE.Mesh(geometry, material);
+// 		sphere.rotation.x = Math.PI / 2;
+// 		sphere.position.set(location.x, location.y, location.z);
+
+// 		// Add event listeners
+// 		sphere.addEventListener('drag', drag);
+// 		sphere.addEventListener('drop', drop);
+// 		sphere.addEventListener('mouseover', mouseover);
+// 		sphere.addEventListener('mouseleave', mouseleave);
+
+// 		potreeViewer.scene.scene.add(sphere);
+// 		spheres.push(sphere);
+// 	});
+// }
 function addMarkers(poleLocations) {
+	let points = []
     poleLocations.forEach(location => {
-        positions.push(location.x, location.y, location.z);
-        const geometry = new THREE.SphereGeometry(.7, 32);
-        const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-        const circle = new THREE.Mesh(geometry, material);
+        points.push(location);
 
-        circle.position.set(location.x, location.y, location.z);
+		const geometry = new THREE.CylinderGeometry(0.2, 0.2, 10, 22);
+        // const geometry = new THREE.CylinderGeometry(0.6, 0.6, 2, 32);
+		// const material = new THREE.MeshBasicMaterial( {color: 0xfff000} ); 
+		const material = new THREE.MeshPhongMaterial({ color: 0xffff00 });
+        const sphere = new THREE.Mesh(geometry, material);
+		sphere.rotation.x = Math.PI / 2;
 
-        potreeViewer.scene.scene.add(circle);
+        sphere.position.set(location.x, location.y, location.z);
+
+        // Add event listeners with arrow functions
+			const drag = (e) => {
+				if (draggingEnabled) {
+					let I = Potree.Utils.getMousePointCloudIntersection(
+						e.drag.end, 
+						e.viewer.scene.getActiveCamera(), 
+						e.viewer, 
+						e.viewer.scene.pointclouds,
+						{pickClipped: true}
+					);
+					console.log('i', I)
+
+					if (I) {
+						let i = spheres.indexOf(e.drag.object);
+						if (i !== -1) {
+							let point = points[i];
+
+							// loop through current keys and cleanup ones that will be orphaned
+							for (let key of Object.keys(point)) {
+								if (!I.point[key]) {
+									delete point[key];
+								}
+							}
+
+							for (let key of Object.keys(I.point).filter(e => e !== 'position')) {
+								point[key] = I.point[key];
+							}
+
+							sphere.position.set(I.location.x, I.location.y, I.location.z);
+							// this.setPosition(i, I.location); // Here, `this` should refer to the instance of the Measure class
+						}
+					}
+				}
+			};
+
+			const drop = (e) => {
+				if (draggingEnabled) {
+					let I = Potree.Utils.getMousePointCloudIntersection(
+						e.drag.end, 
+						e.viewer.scene.getActiveCamera(), 
+						e.viewer, 
+						e.viewer.scene.pointclouds,
+						{pickClipped: true}
+					);
+					// let I = spheres.indexOf(e.drag.object);
+					if (I) {
+						let i = spheres.indexOf(e.drag.object);
+						if (i !== -1) {
+							let point = points[i];
+
+							// loop through current keys and cleanup ones that will be orphaned
+							for (let key of Object.keys(point)) {
+								if (!I.point[key]) {
+									delete point[key];
+								}
+							}
+
+							for (let key of Object.keys(I.point).filter(e => e !== 'position')) {
+								point[key] = I.point[key];
+							}
+
+							sphere.position.set(I.location.x, I.location.y, I.location.z);
+							// this.setPosition(i, I.location); // Here, `this` should refer to the instance of the Measure class
+						}
+					}
+				}
+				// if (i !== -1) {
+				//     this.dispatchEvent({
+				//         'type': 'marker_dropped',
+				//         'measurement': this,
+				//         'index': i
+				//     });
+				// }
+			};
+
+        const mouseover = (e) => e.object.material.emissive.setHex(0xff0000);
+        const mouseleave = (e) => e.object.material.emissive.setHex(0x000000);
+
+        sphere.addEventListener('drag', drag);
+        sphere.addEventListener('drop', drop);
+        sphere.addEventListener('mouseover', mouseover);
+        sphere.addEventListener('mouseleave', mouseleave);
+
+        potreeViewer.scene.scene.add(sphere);
+        spheres.push(sphere);
     });
 }
 
-console.log(positions)
+// function addMarkers(poleLocations) {
+//     poleLocations.forEach(location => {
+//         positions.push(location.x, location.y, location.z);
+//         const geometry = new THREE.SphereGeometry(.7, 32);
+//         const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+//         const circle = new THREE.Mesh(geometry, material);
+// 		// const marker = new Potree.measure();
+
+//         circle.position.set(location.x, location.y, location.z);
+
+//         potreeViewer.scene.scene.add(circle);
+//     });
+// }
+
+// console.log(positions)
 
 // function addMarkers(poleLocations) {
 //     const geometry = new THREE.BufferGeometry();
